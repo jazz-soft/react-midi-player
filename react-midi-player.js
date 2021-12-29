cfb01029db91afd3b336210529604a52eeb7ee2b
Sema <jazz-dev@jazz-soft.net>
2021-12-29 14:11:00 -0500

    npm module

--- react-midi-player.js
+++ react-midi-player.js
@@ -1,11 +1,23 @@
 'use strict';
+(function(global, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory();
+  }
+  else {
+    if (!global) global = window;
+    global.MidiPlayer = factory();
+  }
+})(this, function() {
 
-function MidiPlayer(props) {
-  const ref = React.useRef(null);
-  var player;
-  React.useEffect(() => {
-    player = JZZ.gui.Player(ref.current);
-    return () => { ref.current.innerHTML = ''; };
-  });
-  return React.createElement('span', { ref: ref });
-}
+  function MidiPlayer(props) {
+    const ref = React.useRef(null);
+    var player;
+    React.useEffect(() => {
+      player = JZZ.gui.Player(ref.current);
+      return () => { ref.current.innerHTML = ''; };
+    });
+    return React.createElement('span', { ref: ref });
+  }
+
+  return MidiPlayer;
+});
