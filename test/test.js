c7d9f0c65ba71074357d5d24054752c118988af8
Sema <jazz-dev@jazz-soft.net>
2021-12-31 01:45:35 -0500

    Webpack test

---
+++ test/test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MidiPlayer from 'react-midi-player';
+ReactDOM.render(<MidiPlayer />, document.getElementById('root'));
---
+++ test/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./test.js",
+  mode: "development",
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
